a45e05961a07b90b1a011b5033ed40400325c64a
Rico Erlangga <ricoerlanggaa@gmail.com>
2025-07-01 01:29:39 +0700

    add daisyui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono as GeistMono } from 'next/font/google';
-import './globals.css';
+import { Inter } from 'next/font/google';
+import '@/styles/globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = GeistMono({
-  variable: '--font-geist-mono',
+const inter = Inter({
+  weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
 });
 
@@ -23,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="id" data-theme="light">
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
